be67eb368bdb41fd65b07a8af8ba5c3f9cb79735
Oikkoi <molivas@estudiante.uc.cl>
2026-09-07 15:20:32 -0300

    Actualizado .gitignore

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import numpy as np
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 from skimage import io
+
+imagen = io.imread("test_image.png")
+print("Imagen cargada")
+img_plot = plt.imshow(imagen)
+print("Se supone que muestra")
+plt.show()
