e4f949e32ac90df57dc936cf95df90326ad47e1d
Oikkoi <molivas@estudiante.uc.cl>
2026-09-07 15:26:08 -0300

    Eliminado test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from skimage import io
-
-imagen = io.imread("test_image.png")
-print("Imagen cargada")
-img_plot = plt.imshow(imagen)
-print("Se supone que muestra")
-plt.show()
