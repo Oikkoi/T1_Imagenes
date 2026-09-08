56b850ee64256d3159e95a9e0658a6a8e91a6e4e
Oikkoi <molivas@estudiante.uc.cl>
2026-09-08 08:35:41 -0300

    primer borrador funciones auxiliares

--- P1.py
+++ P1.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
 
+"""Recibe un conjunto arbitrario de pares h y m"""
+
 
 def ColorSaturation():
     pass
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # En esta versión del código, no hay código 
+
+Enlace al repo: [Enlace](https://github.com/Oikkoi/T1_Imagenes.git)
