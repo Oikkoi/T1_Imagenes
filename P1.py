32f89074b2df9262ce3aafb3945b137e87a15d39
Oikkoi <molivas@estudiante.uc.cl>
2026-09-08 11:30:49 -0300

    Eliminamos auxiliar_P1.py y presentamos las funciones para HSV v1

--- P1.py
+++ P1.py
@@ -1,9 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import io
-
-"""Recibe un conjunto arbitrario de pares h y m"""
+from skimage import io, color
 
 
-def ColorSaturation():
-    pass
+def formar_intervalos(lista_tuplas):
+    """El formato requerido para la función es una lista que
+    contiene tuplas con los pares ordenados (h, m)"""
+    puntos = sorted(lista_tuplas, key=lambda tupla: tupla[0])
+    h_tupla, m_tupla = zip(*puntos)
+    h_array = np.array(h_tupla)
+    m_array = np.array(m_tupla)
+    return h_array, m_array
+
+
+def interpolacion(x, h_array, m_array):
+    # Gracias a la periodicidad de la función, no problemas de continuidad en 0 y 1 (ambos son rojo)
+    return np.interp(x, h_array, m_array, period=1)
+
+
+def gm_S(S, matriz_multiplicar):
+    # En np, S*m si ambos son arrays del mismo tamaño es una multiplicación píxel a píxel
+    return np.clip(S * matriz_multiplicar, 0, 1)
+
+
+def ColorSaturationHSV(file: str, lista_tuplas):
+    h_array, m_array = formar_intervalos(lista_tuplas)
+
+    # Definimos los tensores de RGB Y HSV
+    imagen_rgb = io.imread(file)
+    imagen_hsv = color.rgb2hsv(imagen_rgb)
+
+    # definimos el hue y la saturacion:
+    hue = imagen_hsv[:, :, 0]
+    saturacion = imagen_hsv[:, :, 1]
+
+    interpolada = interpolacion(hue, h_array, m_array)
+    nuevo_S = gm_S(saturacion, interpolada)
+    imagen_hsv[:, :, 1] = nuevo_S
+    imagen_rgb = color.hsv2rgb(imagen_hsv)
+    plt.imshow(imagen_rgb)
+    plt.show()
+    return imagen_rgb
+
+
+ColorSaturationHSV("test_image.png", [(0.166, 0), (1, 7), (0.666, 0)])
